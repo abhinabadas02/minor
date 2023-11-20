1a34ab223b2a2a008013ea22107ac6082697034e
abhinabadas02 <122667939+abhinabadas02@users.noreply.github.com>
2023-11-20 15:45:15 +0530

    AboutUs

---
+++ aboutus.js
@@ -0,0 +1,46 @@
+//     var i = 0;
+//     var txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, atque officia facere, aliasmaiores, architecto a aspernatur molestiae id corrupti magni ad placeat fuga quaerat ipsa amet! Aperiameveniet optio numquam ducimus ipsa nam. Magni libero nisi necessitatibus et natus earum inventore debitis?Dolore, numquam fugiat labore eaque perferendis excepturi vitae quaerat saepe ipsa minima accusamus a nisitotam architecto voluptatibus dolorum debitis autem sit laborum tempore doloribus ut dolorem? Enim eavoluptates aut et! Nostrum libero laboriosam, labore laudantium beatae rem cum error perspiciatis itaqueinventore nulla quaerat sunt voluptatibus. Consectetur enim dicta omnis nisi ipsa? Accusamus libero nisienim ipsum velit cum nesciunt sequi illum nulla, dolor provident asperiores dolorem nobis similique ducimus tempora eaque aut dolorum aliquid repellat modi deserunt ipsa.Laborum ipsam quo eius sapiente omnislaboriosam facere vitae sit obcaecati ab repellat beatae distinctio at ducimus laudantium illo fuga liberoofficia possimus maiores praesentium molestiae, illum ipsa.Vel, ratione aperiam tempore tenetur distinctioeius quis velit voluptate nihil quia quod, voluptatibus a labore illum corporis at totam omnis modi!Voluptas mollitia ad rem inventore harum in provident tempore, nemo voluptates sequi.A tempora illum quisexercitationem totam consectetur, cupiditate facilis reiciendis at rerum aut aperiam.';
+//     var speed = 25;
+
+//     function typeWriter(){
+//     if (i < txt.length) {
+//         document.getElementById("typed").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
+
+// const typeWriter = document.getElementById("typed");
+
+// typeWriter.addEventListener(
+//     'load',
+//     () => {
+//         var i = 0;
+//              var txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, atque officia facere, aliasmaiores, architecto a aspernatur molestiae id corrupti magni ad placeat fuga quaerat ipsa amet! Aperiameveniet optio numquam ducimus ipsa nam. Magni libero nisi necessitatibus et natus earum inventore debitis?Dolore, numquam fugiat labore eaque perferendis excepturi vitae quaerat saepe ipsa minima accusamus a nisitotam architecto voluptatibus dolorum debitis autem sit laborum tempore doloribus ut dolorem? Enim eavoluptates aut et! Nostrum libero laboriosam, labore laudantium beatae rem cum error perspiciatis itaqueinventore nulla quaerat sunt voluptatibus. Consectetur enim dicta omnis nisi ipsa? Accusamus libero nisienim ipsum velit cum nesciunt sequi illum nulla, dolor provident asperiores dolorem nobis similique ducimus tempora eaque aut dolorum aliquid repellat modi deserunt ipsa.Laborum ipsam quo eius sapiente omnislaboriosam facere vitae sit obcaecati ab repellat beatae distinctio at ducimus laudantium illo fuga liberoofficia possimus maiores praesentium molestiae, illum ipsa.Vel, ratione aperiam tempore tenetur distinctioeius quis velit voluptate nihil quia quod, voluptatibus a labore illum corporis at totam omnis modi!Voluptas mollitia ad rem inventore harum in provident tempore, nemo voluptates sequi.A tempora illum quisexercitationem totam consectetur, cupiditate facilis reiciendis at rerum aut aperiam.';
+//              var speed = 25;
+//         if (typeWriter.value.match(txt)) {
+//             document.getElementById("typed").innerHTML += txt.charAt(i);
+//             i++;
+//             setTimeout(typeWriter, speed);
+//         }
+//     }
+// )
+
+
+
+
+
+
+
+
+
+// function typeWriter() {
+//     var i = 0;
+//     // var txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, atque officia facere, aliasmaiores, architecto a aspernatur molestiae id corrupti magni ad placeat fuga quaerat ipsa amet! Aperiameveniet optio numquam ducimus ipsa nam. Magni libero nisi necessitatibus et natus earum inventore debitis?Dolore, numquam fugiat labore eaque perferendis excepturi vitae quaerat saepe ipsa minima accusamus a nisitotam architecto voluptatibus dolorum debitis autem sit laborum tempore doloribus ut dolorem? Enim eavoluptates aut et! Nostrum libero laboriosam, labore laudantium beatae rem cum error perspiciatis itaqueinventore nulla quaerat sunt voluptatibus. Consectetur enim dicta omnis nisi ipsa? Accusamus libero nisienim ipsum velit cum nesciunt sequi illum nulla, dolor provident asperiores dolorem nobis similique ducimus tempora eaque aut dolorum aliquid repellat modi deserunt ipsa.Laborum ipsam quo eius sapiente omnislaboriosam facere vitae sit obcaecati ab repellat beatae distinctio at ducimus laudantium illo fuga liberoofficia possimus maiores praesentium molestiae, illum ipsa.Vel, ratione aperiam tempore tenetur distinctioeius quis velit voluptate nihil quia quod, voluptatibus a labore illum corporis at totam omnis modi!Voluptas mollitia ad rem inventore harum in provident tempore, nemo voluptates sequi.A tempora illum quisexercitationem totam consectetur, cupiditate facilis reiciendis at rerum aut aperiam.';
+//     var speed = 25;
+//     if (i < txt.length) {
+//         document.getElementById("typed").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
